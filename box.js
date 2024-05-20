e0731b8c4817fe707323e785dacf1df84644edfa
Anushka Mishra <165766043+anush-mish@users.noreply.github.com>
2024-05-21 04:57:07 +0530

    Add files via upload

--- box.js
+++ box.js
@@ -1,49 +1,23 @@
-gsap.from("#page1 #box",{
-    scale:0,
-    delay:0.2,
-    duration:0.8,
-    rotate:360,
-    backgroundColor:"yellow",
-    borderRadius:"50%",
-    scrollTrigger:{
-        trigger:"#page1 #box",
-        scroller:"body",
-        markers:true,
-        start:"top 70%",
-        scrub:2
-    }
+var path=`M  10 100 Q 500 100 990 100`;
+var finalPath=`M  10 100 Q 500 100 400 100`;
+
+var string=document.querySelector('#string');
+
+string.addEventListener("mousemove",function(dets){
+    path=`M  10 100 Q 500 ${dets.y} 990 100`
+    
+    console.log(path)
+
+    gsap.to("svg path",{
+        attr:{d:path},
+        duration:0.3,
+        ease:"power3.out",
+
+    })
 })
 
-gsap.from("#page2 #box",{
-    scale:0,
-    // delay:1,
-    duration:0.8,
-    rotate:360,
-    backgroundColor:"red",
-    borderRadius:"50%",
-    // scrollTrigger:"#page2 #box"
-    scrollTrigger:{
-        trigger:"#page2 #box",
-        scroller:"body",
-        markers:true,
-        start:"top 70%",
-        scrub:2
-    }
-})
-
-gsap.from("#page3 #box",{
-    scale:0,
-    delay:0.2,
-    duration:0.8,
-    rotate:360,
-    backgroundColor:"blue",
-    borderRadius:"50%",
-    // scrollTrigger:"#page3 #box"
-    scrollTrigger:{
-        trigger:"#page3 #box",
-        scroller:"body",
-        markers:"true",
-        start:"top 70%",
-        scrub:2
-    }
+string.addEventListener("mouseleave",function(){
+    gsap.to("svg path",{
+        attr:{d:finalPath}
+    })
 })
